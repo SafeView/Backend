8f105721f3671349ed6abeb8923ddbc344fcef86
geg222 <gl021414@naver.com>
2025-07-07 11:44:44 +0900

    기본 세팅

---
+++ src/main/java/com/safeview/SafeviewApplication.java
@@ -0,0 +1,13 @@
+package com.safeview;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafeviewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafeviewApplication.class, args);
+	}
+
+}
