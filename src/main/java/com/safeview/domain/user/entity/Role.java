7e47528b1d36cca000883e931cc04389cc6e6a19
shinmink <sinmk99@hansung.ac.kr>
2025-07-22 13:48:43 +0900

    ADD : entity 항목 추가 FEAT : 성별, 역할 enum 으로 분리

---
+++ src/main/java/com/safeview/domain/user/entity/Role.java
@@ -0,0 +1,10 @@
+package com.safeview.domain.user.entity;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum Role {
+    USER,ADMIN
+}
