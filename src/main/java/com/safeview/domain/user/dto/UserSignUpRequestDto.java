9fb1f1f58b7a90887a134d32a1700b6a0a6533d0
원준영 <147246023+geg222@users.noreply.github.com>
2025-10-04 14:35:25 +0900

    fix: 이메일 검증 로직 삭제

--- src/main/java/com/safeview/domain/user/dto/UserSignUpRequestDto.java
+++ src/main/java/com/safeview/domain/user/dto/UserSignUpRequestDto.java
@@ -41,8 +41,4 @@ public class UserSignUpRequestDto {
 
     @NotBlank(message = "생년월일은 필수 항목입니다.")
     private String birthday;
-
-    @NotBlank(message = "이메일 인증번호는 필수 항목입니다.")
-    @Pattern(regexp = "^[0-9]{6}$", message = "인증번호는 6자리 숫자여야 합니다.")
-    private String emailVerificationCode;
 }
